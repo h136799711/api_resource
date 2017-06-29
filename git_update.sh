0c348bd1f2e48408bdbfd7a4b4a8fc17d28f90bd
hebidu <hebiduhebi@126.com>
2017-06-29 17:12:39 +0800

    【完善】增加登录、注册接口

--- git_update.sh
+++ git_update.sh
@@ -1,2 +1,2 @@
 #更新代码仓库
-git fetch origin master:refs/remotes/origin/master && git reset --hard origin/master && composer update
+git fetch origin master:refs/remotes/origin/master && git reset --hard origin/master && composer install && composer update
