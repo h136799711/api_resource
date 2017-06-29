26742dce56cad8d0f14bc541148ae4eeabfbf7a6
hebidu <hebiduhebi@126.com>
2017-06-29 15:56:17 +0800

    【完善】完善应用的接口

--- application/app/config.php
+++ application/app/config.php
@@ -6,6 +6,19 @@
  * Time: 19:59
  */
 return [
+    'exception_handle'       => '\\app\\src\\base\\exception\\JsonExceptionHandler',
+    'default_return_type'    => 'json',
+    // 异常页面的模板文件
+    'exception_tmpl'         => APP_PATH . 'index' . DS . 'view/exception.json',
+    // 异常处理忽略的错误类型，支持PHP所有的错误级别常量，多个级别可以用|运算法
+    // 参考：http://php.net/manual/en/errorfunc.constants.php
+    'exception_ignore_type'  => 0,
+    // 错误显示信息,非调试模式有效
+    'error_message'          => '页面错误！请稍后再试～',
+    // 错误定向页面
+    'error_page'             => '',
+    // 显示错误信息
+    'show_error_msg'         => false,
     //mysql session配置
     'session'                => [
         // SESSION_ID的提交变量,解决flash上传跨域
