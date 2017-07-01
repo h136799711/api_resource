7af5e96fcabbe44f4cc96fed4f3311b42e99738c
hebidu <hebiduhebi@126.com>
2017-07-01 09:51:48 +0800

    【完善】增加应用配置接口

---
+++ application/domain/ClientsConfigDomain.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 2017-06-14
+ * Time: 16:25
+ */
+
+namespace app\domain;
+use app\src\clients\action\ClientsConfigCreateAction;
+use app\src\clients\logic\ClientsConfigLogic;
+
+/**
+ * Class ClientsConfigDomain
+ * 应用配置
+ * @package app\domain
+ */
+class ClientsConfigDomain extends BaseDomain
+{
+    public function create(){
+        $entity = $this->getParams(['title','value','type','app_id','module_code','name']);
+        $result = (new ClientsConfigCreateAction())->create($entity);
+        return $result;
+    }
+
+    public function del(){
+
+    }
+}
