e6e8e26bff9beb3021a63a2979846cc638bc8caa
hebidu <hebiduhebi@126.com>
2017-07-01 09:07:14 +0800

    【完善】增加应用配置接口

---
+++ application/app/controller/ClientsConfig.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 2017-06-30
+ * Time: 16:22
+ */
+
+namespace app\app\controller;
+
+
+class ClientsConfig extends App
+{
+    public function create(){
+
+    }
+
+}
