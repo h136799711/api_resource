e8bedd776ad6c4797ec15a07ec6a16c9a41a74ad
hebidu <hebiduhebi@126.com>
2017-07-03 18:59:24 +0800

    【完善】增加javforme网页读取

--- application/task/controller/BaiduTranslate.php
+++ application/task/controller/BaiduTranslate.php
@@ -11,6 +11,10 @@ namespace app\task\controller;
 
 use app\src\baidu_api\fanyi\BDTranslateLangType;
 use app\src\baidu_api\fanyi\BDTranslater;
+use app\src\base\helper\PageHelper;
+use app\src\qqav\action\ActorAction;
+use app\src\qqav\action\LogAction;
+use app\src\qqav\logic\ActorLogic;
 use think\Controller;
 
 class BaiduTranslate extends Controller
@@ -22,7 +26,27 @@ class BaiduTranslate extends Controller
      * 翻译演员姓名
      */
     public function actor_name(){
+        $order = "id asc";
+        $map = ['name_jp'=>''];
+        $page = ['page_index'=>0,'page_size'=>5];
+        $result = (new ActorAction())->query($map,PageHelper::renew($page),$order);
+        $info = $result['info'];
+        if(array_key_exists("list",$info) && count($info['list']) > 0){
 
+            $translater = (new BDTranslater($this->appId,$this->appSecret));
+            foreach ($info['list'] as $actor){
+                $id = $actor['id'];
+                $en = $actor['name'];
+                $result = $translater->translate($en,BDTranslateLangType::En,BDTranslateLangType::Jp);
+                if(array_key_exists("trans_result",$result)){
+                    $trans_result = $result['trans_result'];
+                    $name_jp = $trans_result['dst'];
+                    (new ActorLogic())->save(['id'=>$id],['name_jp'=>$name_jp]);
+                }elseif(array_key_exists("error_code",$result)){
+                    LogAction::debug($result['error_msg']);
+                }
+            }
+        }
     }
 
     /**
