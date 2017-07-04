34896092f3574ca7c398f3ec1031d54ab80a96cd
hebidu <hebiduhebi@126.com>
2017-07-04 21:58:31 +0800

    【完善】增加自定义命令行

--- application/command.php
+++ application/command.php
@@ -9,5 +9,6 @@ return [
     'app\task\command\Test',
     'app\task\command\Translate',
     'app\task\command\TranslateVideoTitle',
-    'app\task\command\JavformeCrawler'
+    'app\task\command\JavformeCrawler',
+    'app\task\command\VideoPicDownloadLocal'
 ];
---
+++ application/task/command/VideoPicDownloadLocal.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 2017-07-04
+ * Time: 21:55
+ */
+
+namespace app\task\command;
+
+
+use app\src\base\helper\PageHelper;
+use app\src\crawler\action\RemotePictureHelper;
+use app\src\qqav\action\LogAction;
+use app\src\qqav\action\VideoAction;
+use app\src\qqav\logic\VideoLogic;
+use think\console\Command;
+use think\console\Input;
+use think\console\input\Option;
+use think\console\Output;
+
+class VideoPicDownloadLocal extends Command
+{
+    protected function configure()
+    {
+        $this->setName('video_pic:download2local')
+            ->addOption('size','',Option::VALUE_OPTIONAL,'translate size',10)
+            ->setDescription('translate video title to chinese');
+
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        $start = time();
+        $output->writeln(date("Y-m-d H:i:s",$start)." start");
+
+        $size = $input->getOption('size');
+        $this->remote2local($size);
+        $elapse = time() - $start;
+        $desc = ( $elapse / 60 ).'m,'.($elapse % 60).'s';
+        $output->writeln(" cost ".$desc);
+    }
+
+    private function remote2local($size=50){
+        $order = "update_time asc";
+        $map = ['local_main_image'=>0];
+        $page = ['page_index'=>0,'page_size'=>50];
+        $result = (new VideoAction())->query($map,PageHelper::renew($page),$order);
+        $info = $result['info'];
+        if(array_key_exists("list",$info) && count($info['list']) > 0){
+            foreach ($info['list'] as $urlItem){
+                $url = $urlItem['main_image'];
+                $result = RemotePictureHelper::download($url);
+                LogAction::logDebugResult($result);
+                if($result['status']){
+                    $id = intval($result['info']);
+                    $map = ['id'=>$urlItem['id']];
+                    $result = (new VideoLogic())->save($map,['local_main_image'=>$id]);
+                    LogAction::logDebugResult($result);
+                }
+            }
+            return 'success';
+        }
+        return  'fail';
+    }
+}
