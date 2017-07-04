68a2dd059804e3df78e07bbdb30db9a7db170a2f
hebidu <hebiduhebi@126.com>
2017-07-04 09:46:42 +0800

    【完善】增加javforme网页读取

--- application/task/controller/Video.php
+++ application/task/controller/Video.php
@@ -59,10 +59,45 @@ class Video extends Controller
         $page = ['page_index'=>$p,'page_size'=>30];
         $result = (new LogLogic())->queryWithPagingHtml($map,PageHelper::renew($page)->queryParam(),"id asc");
         $info = $result['info'];
-//        $info['list'] = $this->process($info['list']);
+        $info['list'] = $this->processLog($info['list']);
         $this->assign('level',$level);
         $this->assign('list',$info['list']);
         $this->assign('pager',$info['show']);
         return $this->fetch();
+    }
+
+    private function processLog($list){
+        foreach ($list as &$vo){
+            $vo['info'] = $this->unicode_decode($vo['info']);
+        }
+        return $list;
+    }
+
+    function unicode_decode($name)
+    {
+        // 转换编码，将Unicode编码转换成可以浏览的utf-8编码
+        $pattern = '/([\w]+)|(\\\u([\w]{4}))/i';
+        preg_match_all($pattern, $name, $matches);
+        if (!empty($matches))
+        {
+            $name = '';
+            for ($j = 0; $j < count($matches[0]); $j++)
+            {
+                $str = $matches[0][$j];
+                if (strpos($str, '\\u') === 0)
+                {
+                    $code = base_convert(substr($str, 2, 2), 16, 10);
+                    $code2 = base_convert(substr($str, 4), 16, 10);
+                    $c = chr($code).chr($code2);
+                    $c = iconv('UCS-2', 'UTF-8', $c);
+                    $name .= $c;
+                }
+                else
+                {
+                    $name .= $str;
+                }
+            }
+        }
+        return $name;
     }
 }
