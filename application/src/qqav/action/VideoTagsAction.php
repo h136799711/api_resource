5c410dcd1977d37dbfd3bc68a98388338715e802
hebidu <hebiduhebi@126.com>
2017-07-04 10:05:34 +0800

    【完善】增加javforme网页读取

--- application/src/qqav/action/VideoTagsAction.php
+++ application/src/qqav/action/VideoTagsAction.php
@@ -10,8 +10,14 @@ namespace app\src\qqav\action;
 
 
 use app\src\base\action\BaseAction;
+use app\src\base\helper\PageHelper;
+use app\src\qqav\logic\VideoTagsLogic;
 
 class VideoTagsAction extends BaseAction
 {
+    public function insertFromVideo(PageHelper $pageHelper){
+        $result = (new VideoTagsLogic())->queryNotProcessVideo($pageHelper->toPageParam());
 
+        return $result;
+    }
 }
--- application/src/qqav/logic/VideoTagsLogic.php
+++ application/src/qqav/logic/VideoTagsLogic.php
@@ -10,8 +10,30 @@ namespace app\src\qqav\logic;
 
 
 use app\src\base\logic\BaseLogic;
+use think\Db;
 
 class VideoTagsLogic extends BaseLogic
 {
+    public function queryNotProcessVideo($page=['page_index'=>0,'page_size'=>10]){
+        $query = Db::table('qqav_video')
+            ->where('id','NOT IN',function($query){
+                $query->table('qqav_video_tags')->field('video_id');
+            });
+        $start = max(intval($page['page_index'])-1,0)*intval($page['page_size']);
 
+        $list = $query -> limit($start,$page['page_size']) ->order('video_id asc') -> select();
+        $query = Db::table('qqav_video')
+            ->where('id','NOT IN',function($query){
+                $query->table('qqav_video_tags')->field('video_id');
+            });
+        $count = $query -> count();
+
+        $data = [];
+        foreach ($list as $vo){
+            if(method_exists($vo,"toArray")){
+                array_push($data,$vo->toArray());
+            }
+        }
+        return $this -> apiReturnSuc(["count" => $count, "list" => $data]);
+    }
 }
