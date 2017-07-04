619eeeebc2d9279af82f0fb1744718c87331822e
hebidu <hebiduhebi@126.com>
2017-07-04 09:59:34 +0800

    【完善】增加javforme网页读取

---
+++ application/src/qqav/logic/VideoTagsLogic.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 2017-07-04
+ * Time: 09:59
+ */
+
+namespace app\src\qqav\logic;
+
+
+use app\src\base\logic\BaseLogic;
+
+class VideoTagsLogic extends BaseLogic
+{
+
+}
---
+++ application/src/qqav/model/VideoTags.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 2017-07-04
+ * Time: 09:58
+ */
+
+namespace app\src\qqav\model;
+
+
+use app\src\base\model\BaseModel;
+
+class VideoTags extends BaseModel
+{
+    protected $table = "qqav_video_tags";
+}
