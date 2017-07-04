dcad98f61c9b38bb98b2c7b7858ce81008ad3742
hebidu <hebiduhebi@126.com>
2017-07-04 10:09:12 +0800

    【完善】增加javforme网页读取

--- application/src/qqav/action/VideoTagsAction.php
+++ application/src/qqav/action/VideoTagsAction.php
@@ -17,6 +17,27 @@ class VideoTagsAction extends BaseAction
 {
     public function insertFromVideo(PageHelper $pageHelper){
         $result = (new VideoTagsLogic())->queryNotProcessVideo($pageHelper->toPageParam());
+        if($result['status'] && count($result['info']['list']) > 0){
+            $now = time();
+            $list = $result['info']['list'];
+            $allEntity = [];
+            foreach ($list as $vo){
+                $video_id = $vo['id'];
+                $tags = explode(",",$vo['tags']);
+                foreach ($tags as $tag){
+                    array_push($allEntity,[
+                        'video_id'=>$video_id,
+                        'create_time'=>$now,
+                        'update_time'=>$now,
+                        'tag_en'=>$tag,
+                        'tag_cn'=>'',
+                    ]);
+                }
+            }
+            if(count($allEntity) > 0){
+                $result = (new VideoTagsLogic())->addAll($allEntity);
+            }
+        }
 
         return $result;
     }
