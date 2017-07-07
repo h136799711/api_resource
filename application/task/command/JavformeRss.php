8b320fecec2dbc1196b2a13c4dd82fb0f8fb003a
hebidu <hebiduhebi@126.com>
2017-07-07 13:52:25 +0800

    【完善】增加视频演员关联表

--- application/task/command/JavformeRss.php
+++ application/task/command/JavformeRss.php
@@ -9,10 +9,7 @@
 namespace app\task\command;
 
 
-use app\src\base\helper\ValidateHelper;
 use app\src\crawler\action\JavformeRssAction;
-use app\src\crawler\constants\CrawlerUrlType;
-use app\src\crawler\logic\CrawlerUrlLogic;
 use app\src\qqav\action\LogAction;
 use think\console\Command;
 use think\console\Input;
@@ -37,33 +34,5 @@ class JavformeRss extends Command
         $elapse = time() - $start;
         $desc = intval(floor( $elapse / 60 )).'m,'.($elapse % 60).'s';
         $output->writeln(" cost ".$desc);
-    }
-
-    private function read($url){
-        $xml = file_get_contents($url);
-        $xml = simplexml_load_string($xml);
-        $items = $xml->xpath("channel/item");
-        $now = time();
-        $allEntity = [];
-        foreach ($items as $item){
-            $url = ''.$item->link;
-            $map = ['url'=>$url];
-            $result = (new CrawlerUrlLogic())->getInfo($map);
-
-            if(ValidateHelper::legalArrayResult($result) && $result['info']['url'] == $url) {
-                //已经存在则不添加了
-                continue;
-            }
-            array_push($allEntity,[
-                'url'=>$url,
-                'create_time'=>$now,
-                'update_time'=>$now,
-                'climb_status'=>0,
-                'url_type'=>CrawlerUrlType::JAV_FOR_ME,
-            ]);
-        }
-        if(count($allEntity) > 0){
-            $result = (new CrawlerUrlLogic())->addAll($allEntity);
-        }
     }
 }
