616c7916e924cfa4a1312e1911be3bcf060c9371
hebidu <hebiduhebi@126.com>
2017-07-19 14:56:29 +0800

    【完善】增加视频演员关联表

--- application/index/controller/Test.php
+++ application/index/controller/Test.php
@@ -53,14 +53,20 @@ class Test extends Controller
 //        $model = new \app\index\model\Test();
         $connection = Db::connect($config);
         $table = $connection->name('test2');
-        $model = new \app\index\model\Test();
+        $logic = new TestLogic();
+//        $model = new \app\index\model\Test();
         foreach ($list as $item){
             $id = $item;
-//            $result = $logic->saveByID($id,['create_time'=>2]);
-//            var_dump($result);
-            $result = $model->save(['create_time'=>6],['id'=>$id]);
-//            $result = $table->where('id',$id)->update(['create_time'=>5]);
+            $result = $logic->saveByID($id,['create_time'=>3]);
             var_dump($result);
+//            $result = $model->save(['create_time'=>2],['id'=>$id]);
+//            $map = ['id'=>$id];
+//            $entity = ['create_time'=>time()];
+//            $result = $model->where($map)->update($entity);
+//
+//            $result = $table->where('id',$id)->update(['create_time'=>5]);
+//            var_dump($model->getLastSql());
+//            var_dump($result);
         }
     }
 
