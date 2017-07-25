2dce2d0155f1cde14433edf74ab8d6f083e7bdaf
hebidu <hebiduhebi@126.com>
2017-07-25 10:58:53 +0800

    【完善】增加数据字典接口

--- application/app/controller/Datatree.php
+++ application/app/controller/Datatree.php
@@ -14,11 +14,16 @@ use app\sdk\req\ByDatatreeRequest;
 class Datatree extends App
 {
     public function query(){
+        $parent_id = $this->_param('parent_id','');
         $level = $this->_param('level','');
         $map = [];
         if(!empty($level)){
             $map['level'] = $level;
         }
+        if(strlen($parent_id) > 0){
+            $map['parentid'] = $parent_id;
+        }
+
         $order = $this->_param('order','level asc');
         $fields = $this->_param('fields','parents,parentid,notes,create_time,update_time,level,iconurl,data_level,sort,alias,id,code,name');
         $pageHelper = $this->getPageHelper();
